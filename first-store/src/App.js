0603fdc98d79e9f280b7ef77a7a6add5e86949af
Egor Danilov <mellowd94@gmail.com>
2023-11-06 20:26:55 +0100

    time 06.11.23 20:30

--- first-store/src/App.js
+++ first-store/src/App.js
@@ -1,16 +1,16 @@
 function App() {
   return (
-    <div className="wrapper">
-      <header>
-        <div className="headerleft">
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
           <img width={80} height={80} src="/img/logo.png" />
-          <div className="headerInfo">
-            <h3>First Store!</h3>
+          <div>
+            <h3 className="text-uppercase">First Store!</h3>
             <p>My first frontend project</p>
           </div>
         </div>
-        <ul className="headerRight">
-          <li>
+        <ul className="d-flex">
+          <li className="mr-30">
             <img width={40} height={40} src="/img/cart.png" />
             <span>Preis</span>
           </li>
